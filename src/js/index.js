45f9a64a6f13959513e980a1b086852a03a343d9
yazidmhd <accelyazid@gmail.com>
2019-07-09 14:48:09 +0800

    setup webpack and babel

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
 // Global app controller
+import num from './test';
+const x = 23;
+console.log(`I imported ${num}. Called test.js, ${x}`);
---
+++ src/js/test.js
@@ -0,0 +1,2 @@
+console.log('Imported mods');
+export default 12343456789;
